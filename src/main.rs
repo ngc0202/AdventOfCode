4a91333c59f310564bdec6d4e8889c904098c770
Nicholas Cyprus <ngc0202@gmail.com>
2021-12-04 18:02:59 -0500

    Day 4 solutions

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+
+use std::error::Error;
+use std::io::{self, BufReader};
+use std::fs::File;
+use std::path::PathBuf;
+
+mod day4;
+
+pub type GenResult = Result<(), Box<dyn Error>>;
+
+
+fn main() -> GenResult {
+    day4::run()?;
+
+	Ok(())
+}
+
+pub fn load_input(day: u8) -> io::Result<BufReader<File>> {
+	let mut path = PathBuf::new();
+	path.push("inputs");
+	path.push(format!("input{:02}.txt", day));
+	println!("Reading from {}", path.display());
+	File::open(&path).map(BufReader::new)
+}
